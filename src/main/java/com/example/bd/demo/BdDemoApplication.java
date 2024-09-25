1e85cb211a60e4b4d67237b4cae0744d8fe9394e
rafshinee <rafaele.00000847840@unicap.br>
2024-09-25 15:22:43 -0300

    dockerfile

---
+++ src/main/java/com/example/bd/demo/BdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdDemoApplication.class, args);
+	}
+
+}
